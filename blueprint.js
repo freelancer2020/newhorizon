e2d0c1d8bcbcf3dce7630a5ff5d2314d9ae7630d
Mostafa <mustafa.husseiny@gmail.com>
2019-10-11 22:33:12 +0200

    Update blueprint.js

--- blueprint.js
+++ blueprint.js
@@ -1,1 +1,19 @@
-
+let i = 0;
+class Mybutton extends HTMLButtonElement {
+  constructor() {
+    super();
+    this.style.cssText = `
+    width: 200px;
+    height: 50px;
+    background-color: lightgreen;
+    color: #ffffff;
+    font-size: 20px;
+   `;
+    this.addEventListener("click", function() {
+      i++;
+      this.setAttribute("value", i);
+      this.textContent = this.getAttribute("value");  
+  }                      
+}
+ customElements.define("my-btn", Mybutton, {extends: "button"});                         
+                          
