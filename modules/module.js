4987daf20bd60ccc1aaa17176eb9ce4823bae6c3
Mostafa <mustafa.husseiny@gmail.com>
2019-10-19 16:35:10 +0200

    Add files via upload

--- modules/module.js
+++ modules/module.js
@@ -1,4 +1,4 @@
-/*
+
 const template = `
 <template id = "rock">
 
@@ -23,20 +23,32 @@ img {
 </style>
 
 `;
-*/
 function render() {
-    /*
-    let axa = document.createElement("div");
-    axa.setAttribute("id", "poster");
-    axa.append(template);
-    document.body.append(axa);
-    let templ = document.getElementById("rock");
-    let hoster = document.importNode(templ.cloneNode, true);
     let container = document.createElement("div");
-    document.body.append(container);
-   // container.append(hoster);
-   */
-    document.bgColor = "red";
+    container.setAttribute("class", "gallery");
+    let myGallery = document.getElementsByClassName("gallery")[0];
+    myGallery.getElementsByClassName.cssText = `
+    width: 100vw;
+    height: 400px;
+    display: flex;
+    flex-direction: flex-row;
+    `;
+    document.body.append(myGallery);
+    let url = "https://raw.githubusercontent.com/freelancer2020/newhorizon/master/modules/g-sm.json";
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(result => {
+        for (let i = 0; i < result.length; i++) {
+            let img = document.createElement("img");
+            img.style.cssText = `width: 100px; height: 100px;`;
+            img[i].src = result[i].src;
+            container.append(img);
+        }
+    })
+
+  document.bgColor = "red";
 }
 
 window.onclick = render;
