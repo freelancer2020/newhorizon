014a2e8cd5421d16314f34418634e6153bdb36bc
Mostafa <mustafa.husseiny@gmail.com>
2019-10-19 15:49:27 +0200

    Add files via upload

---
+++ modules/module.js
@@ -0,0 +1,34 @@
+const template = `
+<template id = "rock">
+
+<div id = "container">
+<div id = "header"> JavaScript Modules </div>
+<img src = "https://coherent-labs.com/posts/wp-content/uploads/2017/10/x6.png.pagespeed.ic.uaP8KPUyaZ.png">
+</div>
+
+<style>
+
+#container {
+    width: 100vw;
+    height: 400px;
+    background-color: #212121;
+}
+
+img {
+    width: 400px;
+    height: auto;
+}
+
+</style>
+
+`;
+function render() {
+    let axa = document.createElement("div");
+    axa.setAttribute("id", "poster");
+    document.body.append(axa);
+    let templ = document.getElementById("rock");
+    let hoster = document.importNode(templ.cloneNode, true);
+    axa.append(hoster);
+}
+
+render();
